5c474fe805b15853d847ae263d5b7b821ca665da
Cauê Melo <cauesmelo@gmail.com>
2020-10-08 10:22:13 -0300

    Initial setup

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import routes from './routes';
+
+const app = express();
+
+app.get('/', (request, response) => response.json({ message: 'Hello' }));
+
+app.listen(3333, () => {
+  console.log('Server started on port 3333.');
+});

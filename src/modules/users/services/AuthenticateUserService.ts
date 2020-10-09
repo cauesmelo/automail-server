d681e663afc8aca1ad8f011fc2d91725cf987669
Cauê Melo <cauesmelo@gmail.com>
2020-10-09 16:13:29 -0300

    add authentication

---
+++ src/modules/users/services/AuthenticateUserService.ts
@@ -0,0 +1,61 @@
+import { injectable, inject } from 'tsyringe';
+import { sign } from 'jsonwebtoken';
+import User from '@modules/users/infra/typeorm/entities/User';
+import AppError from '@shared/errors/AppError';
+import configAuth from '@config/auth';
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: User;
+  token: string;
+}
+
+@injectable()
+export default class AuthenticateUserService {
+  constructor(
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository,
+
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
+  ) {}
+
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new AppError('Incorrect email/password combination.');
+    }
+
+    const passwordMatched = await this.hashProvider.compareHash(
+      password,
+      user.password,
+    );
+
+    if (!passwordMatched) {
+      throw new AppError('Incorrect email/password combination.');
+    }
+
+    // eslint-disable-next-line
+    // @ts-ignore
+    delete user.password;
+
+    const { secret, expiresIn } = configAuth.jwt;
+
+    const token = sign({}, secret, {
+      subject: user.id,
+      expiresIn,
+    });
+
+    return {
+      user,
+      token,
+    };
+  }
+}
--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -2,7 +2,11 @@ import { Router } from 'express';
 
 import usersRouter from '@modules/users/infra/http/routes/users.routes';
 
+import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
+
 const routes = Router();
+
+routes.use('/sessions', sessionsRouter);
 
 routes.use('/users', usersRouter);
 

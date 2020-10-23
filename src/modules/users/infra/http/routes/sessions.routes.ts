1b686bb9bc73e6ab66afc482d179faaadfe0b968
Cauê Melo <cauesmelo@gmail.com>
2020-10-23 20:08:41 -0300

    Rewrite in AuthenticateUserService, routes and controller.

--- src/modules/users/infra/http/routes/sessions.routes.ts
+++ src/modules/users/infra/http/routes/sessions.routes.ts
@@ -1,21 +1,10 @@
 import { Router } from 'express';
-import { celebrate, Segments, Joi } from 'celebrate';
 
 import SessionsController from '@modules/users/infra/http/controller/SessionsController';
 
 const sessionsRouter = Router();
 const sessionsController = new SessionsController();
-sessionsRouter.post(
-  '/',
 
-  celebrate({
-    [Segments.BODY]: {
-      email: Joi.string().email().required(),
-      password: Joi.string().required(),
-    },
-  }),
-
-  sessionsController.create,
-);
+sessionsRouter.post('/', sessionsController.create);
 
 export default sessionsRouter;

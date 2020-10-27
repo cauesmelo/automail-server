5360ce5404f8b64f71a5bd2b1455d17b26f8ad65
Cauê Melo <cauesmelo@gmail.com>
2020-10-27 08:22:30 -0300

    Remove unused parts of code

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -6,20 +6,12 @@ import './providers';
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
-import IListsRepository from '@modules/lists/repositories/IListsRepository';
-import ListsRepository from '@modules/lists/infra/typeorm/repositories/ListsRepository';
-
 import IEmailsRepository from '@modules/emails/repositories/IEmailsRepository';
 import EmailsRepository from '@modules/emails/infra/typeorm/repositories/EmailsRepository';
 
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository,
-);
-
-container.registerSingleton<IListsRepository>(
-  'ListsRepository',
-  ListsRepository,
 );
 
 container.registerSingleton<IEmailsRepository>(
